dd233db9b26a193f0cf6ac7ef595fb71aa341554
Daniel Baez <jbaez0@ucol.mx>
2026-09-06 00:40:19 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("================================\n");
+    printf("¡HOLA, GITHUB!\n");
+    printf("================================\n");
+    printf("Mi nombre es: Daniel Baez\n");
+    printf("Estoy aprendiendo programación.\n");
+    printf("Este es mi primer programa\n");
+    printf("publicado en GitHub.\n");
+    printf("¡Bienvenido a mi repositorio!\n");
+
+    return 0;
+}
